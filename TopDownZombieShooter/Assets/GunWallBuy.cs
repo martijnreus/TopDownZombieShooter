4d007ada431764116c648ed24e86383032d99f73
martijnreus <martijn.reus@gmail.com>
2023-09-11 18:34:15 +0200

    Organized the folder

---
+++ TopDownZombieShooter/Assets/GunWallBuy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunWallBuy : MonoBehaviour
+{
+    [SerializeField] private Gun gun;
+
+    private bool canInteract;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
