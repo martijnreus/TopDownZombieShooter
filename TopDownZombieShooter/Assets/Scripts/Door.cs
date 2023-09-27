330f9b5493e7f5b962f372e0e281a3220f7a5953
martijnreus <martijn.reus@gmail.com>
2023-09-27 18:28:15 +0200

    Added a buyable door system

---
+++ TopDownZombieShooter/Assets/Scripts/Door.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] int doorCost;
+
+    private PointManager pointManager;
+
+    private void Awake()
+    {
+        pointManager = FindObjectOfType<PointManager>();
+    }
+
+    public void Interact()
+    {
+        if (pointManager.GetCurrentPointAmount() >= doorCost) 
+        {
+            pointManager.RemovePoints(doorCost);
+            OpenDoor();
+        }
+    }
+
+    public string GetInteractText()
+    {
+        return "Press E to open door \n[Cost: " + doorCost + "]";
+    }
+
+    private void OpenDoor()
+    {
+        // placeholder replace with actual implementation of opening a door
+        gameObject.SetActive(false);
+    }
+}
