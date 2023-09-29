176fb5030a4af201195a58a51173db7bf02e26d8
martijnreus <martijn.reus@gmail.com>
2023-09-29 18:45:23 +0200

    The player now slowly heals after being damaged

--- TopDownZombieShooter/Assets/Scripts/PlayerScripts/Player.cs
+++ TopDownZombieShooter/Assets/Scripts/PlayerScripts/Player.cs
@@ -6,12 +6,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private int health;
+    [SerializeField] private float healDelay;
+    [SerializeField] private float healRate;
+
+    private float timeLastDamaged;
+    private float timeLastHealed;
 
     public HealthSystem healthSystem;
 
     private void Awake()
     {
         healthSystem = new HealthSystem(health);
+        healthSystem.OnDamaged += OnDamaged;
+    }
+
+    private void OnDamaged(object sender, EventArgs e)
+    {
+        timeLastDamaged = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - timeLastDamaged > healDelay && Time.time - timeLastHealed > healRate)
+        {
+            timeLastHealed = Time.time;
+            healthSystem.Heal(1);
+        }
     }
 
     public HealthSystem GetHealthSystem()
