9648a7797a47e15aeee2f1739853e998d8ad73d9
martijnreus <martijn.reus@gmail.com>
2023-10-22 18:00:57 +0200

    Added a test main menu

---
+++ TopDownZombieShooter/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
