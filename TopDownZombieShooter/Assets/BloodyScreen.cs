b14f138baee157469791b6f9f2277f5cbdbd0f4c
martijnreus <martijn.reus@gmail.com>
2023-10-23 20:16:02 +0200

    Changed the blood screen logic

--- TopDownZombieShooter/Assets/BloodyScreen.cs
+++ TopDownZombieShooter/Assets/BloodyScreen.cs
@@ -7,40 +7,25 @@ public class BloodyScreen : MonoBehaviour
 {
     private Player player;
 
-    private float alphaAmount = 0f;
-    private float timeDamaged;
-
     [SerializeField] private Image bloodyScreen;
-    [SerializeField] private float fadeSpeed;
-    [SerializeField] private float fadeDelay;
 
     private void Start()
     {
         player = FindObjectOfType<Player>();
-        player.GetHealthSystem().OnDamaged += BloodyScreen_OnDamaged;
-    }
-
-    private void BloodyScreen_OnDamaged(object sender, System.EventArgs e)
-    {
-        alphaAmount = 1f - (player.GetHealthSystem().GetHealthNormalized() / 2);
-        timeDamaged = Time.time;
     }
 
     private void Update()
     {
-        FadeBloodEffect();
-        UpdatePictureAlpha();
+        float alphaAmount = CalculateAlpha();
+        SetPictureAlpha(alphaAmount);
     }
 
-    private void FadeBloodEffect()
+    private float CalculateAlpha()
     {
-        if (Time.time - timeDamaged > fadeDelay)
-        {
-            alphaAmount -= fadeSpeed * Time.deltaTime;
-        }
+        return 1f - Mathf.Pow(player.GetHealthSystem().GetHealthNormalized(), 2);
     }
 
-    private void UpdatePictureAlpha()
+    private void SetPictureAlpha(float alphaAmount)
     {
         bloodyScreen.color = new Color(1, 1, 1, alphaAmount);
     }
