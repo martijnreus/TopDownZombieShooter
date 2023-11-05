48e4b6728bdb6f90b6a312503acfa34d4f2bce88
martijnreus <martijn.reus@gmail.com>
2023-11-05 20:13:24 +0100

    Now able to play random sound in a random time interval

---
+++ TopDownZombieShooter/Assets/Scripts/BackgroundSound.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour
+{
+    [SerializeField] AudioClip[] audioToPlay;
+    [SerializeField] float minTimeNextSound;
+    [SerializeField] float maxTimeNextSound;
+
+    private float randomSoundTimer;
+
+    private void Awake()
+    {
+        ResetRandomTimer();
+    }
+
+    private void Update()
+    {
+        Debug.Log(randomSoundTimer);
+        randomSoundTimer -= Time.deltaTime;
+
+        if (randomSoundTimer <= 0) 
+        {
+            PlayRandomSound();
+            ResetRandomTimer();
+        }
+    }
+
+    private void ResetRandomTimer()
+    {
+        randomSoundTimer = Random.Range(minTimeNextSound, maxTimeNextSound);
+    }
+
+    private void PlayRandomSound()
+    {
+        SoundManager.PlaySound(audioToPlay[Random.Range(0, audioToPlay.Length - 1)]);
+    }
+}
