ef40dd244cc23b163828d039c5db8621fe29e533
martijnreus <martijn.reus@gmail.com>
2023-11-25 00:16:38 +0100

    Statistics menu is now nice

--- TopDownZombieShooter/Assets/MenuTween.cs
+++ TopDownZombieShooter/Assets/MenuTween.cs
@@ -17,21 +17,37 @@ public class MenuTween : MonoBehaviour
     [Header("Settings Menu")]
     [SerializeField] private GameObject settingsMrnu;
 
-    private float menuTweenSpeed = 1f;
+    private float menuTweenSpeed = 0.75f;
+
+    Vector2 mainMenuStartPosition;
+    Vector2 mainMenuTextPosition;
+
+    private void Start()
+    {
+        mainMenuStartPosition = mainMenuPanel.transform.localPosition;
+        mainMenuTextPosition = mainMenuTitle.transform.localPosition;
+    }
 
     public void OpenStatistics()
     {
-        float openDelay = 0.2f;
+        float openDelay = 0.1f;
 
         statisticsMenuCanvasGroup.alpha = 1f;
 
         statisticsMenu.transform.localPosition = new Vector3(0f, -1000f, 0f);
         statisticsMenu.transform.DOLocalMoveY(0f, menuTweenSpeed).SetEase(Ease.InOutBack).SetDelay(openDelay);
+
+        statisticsMenuCanvasGroup.DOFade(1f, menuTweenSpeed).SetEase(Ease.InOutBack);
     }
 
     public void CloseStatistics()
     {
+        statisticsMenuCanvasGroup.alpha = 1f;
 
+        statisticsMenu.transform.localPosition = new Vector3(0f, 0f, 0f);
+        statisticsMenu.transform.DOLocalMoveY(-1000f, menuTweenSpeed).SetEase(Ease.InOutBack);
+
+        statisticsMenuCanvasGroup.DOFade(0f, menuTweenSpeed * 2).SetEase(Ease.InOutBack);
     }
 
     public void OpenSettings()
@@ -46,7 +62,17 @@ public class MenuTween : MonoBehaviour
 
     public void OpenMainMenu()
     {
+        float openDelay = 0.1f;
 
+        mainMenuCanvasGroup.alpha = 0f;
+
+        mainMenuTitle.transform.localPosition = new Vector3(mainMenuTextPosition.x, 1000f, 0f);
+        mainMenuTitle.transform.DOLocalMoveY(mainMenuTextPosition.y, menuTweenSpeed).SetEase(Ease.InOutBack).SetDelay(openDelay);
+
+        mainMenuPanel.transform.localPosition = new Vector3(2000f, mainMenuStartPosition.y, 0f);
+        mainMenuPanel.transform.DOLocalMoveX(mainMenuStartPosition.x, menuTweenSpeed).SetEase(Ease.InOutBack).SetDelay(openDelay);
+
+        mainMenuCanvasGroup.DOFade(1f, menuTweenSpeed).SetEase(Ease.InOutBack);
     }
 
     public void CloseMainMenu()
